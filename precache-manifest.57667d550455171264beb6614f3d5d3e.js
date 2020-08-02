0cd3d7a3ae177a44e4571285b09a00c94e79532c
msplawn <mmsplawn@gmail.com>
2020-08-01 21:27:22 -0500

    Updates

---
+++ precache-manifest.57667d550455171264beb6614f3d5d3e.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7f2e937f925e74c8014ac7c022d47a3",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "a13b2287ace6268287b3",
+    "url": "/react-portfolio/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "6c7b082bea797d944359",
+    "url": "/react-portfolio/static/js/2.a43fc942.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-portfolio/static/js/2.a43fc942.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a13b2287ace6268287b3",
+    "url": "/react-portfolio/static/js/main.476ef6b3.chunk.js"
+  },
+  {
+    "revision": "8e59a2d6dc418ca17627",
+    "url": "/react-portfolio/static/js/runtime-main.62a8e445.js"
+  },
+  {
+    "revision": "f2f2506e20ee5c8b817be45b86e058ae",
+    "url": "/react-portfolio/static/media/budget.f2f2506e.png"
+  },
+  {
+    "revision": "ce7c5bdb725c8bee57c4eb6e90b89232",
+    "url": "/react-portfolio/static/media/burger.ce7c5bdb.png"
+  },
+  {
+    "revision": "a6708291cd948d29484e4854990d5d5c",
+    "url": "/react-portfolio/static/media/contact.a6708291.png"
+  },
+  {
+    "revision": "450d0b1bb55aa1ee2907097cbc396dfd",
+    "url": "/react-portfolio/static/media/dayplanner.450d0b1b.png"
+  },
+  {
+    "revision": "0f40328642b780911b70366092ed793a",
+    "url": "/react-portfolio/static/media/morgan-splawn-resume.0f403286.pdf"
+  },
+  {
+    "revision": "da8c80c2a49178628409a2f4673b3a52",
+    "url": "/react-portfolio/static/media/morgan.da8c80c2.png"
+  },
+  {
+    "revision": "3ba7b13204542c98d6f2adfb4b93c5d7",
+    "url": "/react-portfolio/static/media/note-taker.3ba7b132.png"
+  },
+  {
+    "revision": "e4ca3b8610d0e0063705d4ac6137cc63",
+    "url": "/react-portfolio/static/media/onhand.e4ca3b86.png"
+  },
+  {
+    "revision": "8418f0bec0e31fbd934f46f2e59a11ea",
+    "url": "/react-portfolio/static/media/simplysomm1.8418f0be.png"
+  }
+]);
